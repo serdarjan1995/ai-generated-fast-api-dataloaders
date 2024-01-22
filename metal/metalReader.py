2a822ed8219f29f9bb937804ccae09d837bac218
Serhii Kosovan <koso.serhii@gmail.com>
2024-01-22 05:05:25 -0500

    feat: OpenAPI in MetalReader

--- metal/metalReader.py
+++ metal/metalReader.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI, HTTPException, Security, Depends
 from fastapi.security.api_key import APIKeyHeader
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/v1/openapi.json")
 
-API_KEY_NAME = 'api_key'
+API_KEY_NAME = "api_key"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 
 
@@ -17,20 +17,23 @@ def get_api_key(api_key_header: str = Depends(api_key_header)):
     return api_key_header
 
 
-@app.post('/load-data', response_model=List[Document])
+@app.post("/load-data", response_model=List[Document])
 def load_data(
-    api_key: str = Depends(get_api_key),
     client_id: str,
     index_id: str,
     limit: int,
     query_embedding: Optional[List[float]] = None,
     filters: Optional[Dict[str, Dict[str, str]]] = None,
-    separate_documents: bool = True
+    api_key: str = Depends(get_api_key),
+    separate_documents: bool = True,
 ):
     try:
         from metal_sdk.metal import Metal
     except ImportError:
-        raise HTTPException(status_code=500, detail="`metal_sdk` package not found, please run `pip install metal_sdk`")
+        raise HTTPException(
+            status_code=500,
+            detail="`metal_sdk` package not found, please run `pip install metal_sdk`",
+        )
 
     metal_client = Metal(api_key, client_id, index_id)
     payload = {
