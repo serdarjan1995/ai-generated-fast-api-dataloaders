ed7d4a724c9d7758f77a17a218677c3077ecb332
Serhii Kosovan <koso.serhii@gmail.com>
2024-01-22 04:48:19 -0500

    feat: OpenAPI in MemosLoader

--- memos/memosLoader.py
+++ memos/memosLoader.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, HTTPException, Query, Depends
 from pydantic import BaseModel
 import requests
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/v1/openapi.json")
 
 
 class Document(BaseModel):
@@ -13,12 +13,12 @@ class Document(BaseModel):
 
 class MemosReader:
     def __init__(self, host: str):
-        self._memoUrl = host.rstrip('/') + '/api/memo'
+        self._memoUrl = host.rstrip("/") + "/api/memo"
 
     def load_data(self, params: Optional[Dict] = None):
         if params is None:
             params = {}
-        realUrl = self._memoUrl + ('/all' if not params else '')
+        realUrl = self._memoUrl + ("/all" if not params else "")
         try:
             response = requests.get(realUrl, params=params)
             response.raise_for_status()
@@ -26,17 +26,31 @@ class MemosReader:
         except requests.RequestException as e:
             raise HTTPException(status_code=400, detail=str(e))
 
-        if 'data' not in data:
-            raise HTTPException(status_code=500, detail='Invalid Memo response')
+        if "data" not in data:
+            raise HTTPException(status_code=500, detail="Invalid Memo response")
 
-        documents = [Document(text=memo['content'], extra_info={'creator': memo['creator'], 'resource_list': memo['resourceList'], id: memo['id'],}) for memo in data['data']]
+        documents = [
+            Document(
+                text=memo["content"],
+                extra_info={
+                    "creator": memo["creator"],
+                    "resource_list": memo["resourceList"],
+                    id: memo["id"],
+                },
+            )
+            for memo in data["data"]
+        ]
         return documents
 
 
 def get_loader(host: str):
     return MemosReader(host)
 
-@app.get('/load_data', response_model=List[Document])
-async def load_data(host: str = Query(..., description='Host where memos is deployed'), params: Optional[Dict] = Query(None, description='Filtering parameters')):
+
+@app.get("/load_data", response_model=List[Document])
+async def load_data(
+    host: str = Query(..., description="Host where memos is deployed"),
+    params: Optional[Dict] = None,
+):
     loader = get_loader(host)
     return loader.load_data(params)
