8ba129e42099c0216e1906f35a1dd3178037eb6b
Serhii Kosovan <koso.serhii@gmail.com>
2024-01-22 04:25:07 -0500

    feat: OpenAPI in MakeLoader

--- make_com/makeLoader.py
+++ make_com/makeLoader.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 import requests
 from typing import Optional, List
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/v1/openapi.json")
 
 
 class NodeWithScoreModel(BaseModel):
@@ -17,16 +17,24 @@ class ResponseModel(BaseModel):
     query: Optional[str] = None
 
 
-@app.post("/pass-response-to-webhook/",
-          summary="Pass response to Make.com webhook",
-          description="Pass a response object to a specified Make.com webhook url.")
-async def pass_response_to_webhook(webhook_url: str = Query(..., description="Webhook URL to which the response will be sent"),
-                               response: ResponseModel = Body(..., description="Response object containing the response and source nodes"),
-                               query: Optional[str] = Body(None, description="Optional query string")):
+@app.post(
+    "/pass-response-to-webhook/",
+    summary="Pass response to Make.com webhook",
+    description="Pass a response object to a specified Make.com webhook url.",
+)
+async def pass_response_to_webhook(
+    webhook_url: str = Query(
+        ..., description="Webhook URL to which the response will be sent"
+    ),
+    response: ResponseModel = Body(
+        ..., description="Response object containing the response and source nodes"
+    ),
+    query: Optional[str] = Body(None, description="Optional query string"),
+):
     json_dict = {
         "response": response.response,
         "source_nodes": [node.dict() for node in response.source_nodes],
-        "query": response.query
+        "query": response.query,
     }
     r = requests.post(webhook_url, json=json_dict)
     try:
@@ -38,4 +46,5 @@ async def pass_response_to_webhook(webhook_url: str = Query(..., description="We
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
