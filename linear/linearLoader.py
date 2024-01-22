cb7ecdb46d3515442595ed74784a29fa3e900b76
Serhii Kosovan <koso.serhii@gmail.com>
2024-01-22 04:19:23 -0500

    feat: OpenAPI in LinearLoader

--- linear/linearLoader.py
+++ linear/linearLoader.py
@@ -4,17 +4,16 @@ from typing import List
 import requests
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/v1/openapi.json")
 
-API_KEY_NAME = 'Authorization'
+API_KEY_NAME = "access_token"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
-
-# Pydantic models
 
 class Document(BaseModel):
     text: str
     extra_info: dict
+
 
 class LinearReader:
     def __init__(self, api_key: str) -> None:
@@ -22,22 +21,23 @@ class LinearReader:
 
     def load_data(self, query: str) -> List[Document]:
         graphql_endpoint = "https://api.linear.app/graphql"
-        headers = {'Authorization': self.api_key, 'Content-Type': 'application/json'}
-        payload = {'query': query}
+        headers = {"Authorization": self.api_key, "Content-Type": "application/json"}
+        payload = {"query": query}
         response = requests.post(graphql_endpoint, json=payload, headers=headers)
         data = response.json()
 
         issues = []
-        team_data = data.get('data', {}).get('team', {})
-        for issue in team_data.get('issues', {}).get('nodes', []):
+        team_data = data.get("data", {}).get("team", {})
+        for issue in team_data.get("issues", {}).get("nodes", []):
             document = Document(
-                text=f"{issue['title']} \n {issue['description']}",
-                extra_info=issue
+                text=f"{issue['title']} \n {issue['description']}", extra_info=issue
             )
             issues.append(document)
         return issues
 
-@app.post('/load_data/', response_model=List[Document])
+
+@app.post("/load_data/", response_model=List[Document])
 async def load_data(query: str, api_key: APIKeyHeader = Security(api_key_header)):
+    print(query)
     linear_reader = LinearReader(api_key=api_key)
     return linear_reader.load_data(query=query)
