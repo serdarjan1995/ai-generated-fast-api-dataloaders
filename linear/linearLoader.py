a0ac417f0f041b4265c7800be355015c26389dde
Serhii Kosovan <koso.serhii@gmail.com>
2024-01-23 03:15:45 -0500

    fix: linearLoader

--- linear/linearLoader.py
+++ linear/linearLoader.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, HTTPException, Security
+from fastapi import FastAPI, Security
 from fastapi.security.api_key import APIKeyHeader
 from typing import List
-import requests
 from pydantic import BaseModel
+import llama_hub.linear as LinearReader
 
 app = FastAPI(openapi_url="/api/v1/openapi.json")
 
@@ -13,27 +13,6 @@ api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 class Document(BaseModel):
     text: str
     extra_info: dict
-
-
-class LinearReader:
-    def __init__(self, api_key: str) -> None:
-        self.api_key = api_key
-
-    def load_data(self, query: str) -> List[Document]:
-        graphql_endpoint = "https://api.linear.app/graphql"
-        headers = {"Authorization": self.api_key, "Content-Type": "application/json"}
-        payload = {"query": query}
-        response = requests.post(graphql_endpoint, json=payload, headers=headers)
-        data = response.json()
-
-        issues = []
-        team_data = data.get("data", {}).get("team", {})
-        for issue in team_data.get("issues", {}).get("nodes", []):
-            document = Document(
-                text=f"{issue['title']} \n {issue['description']}", extra_info=issue
-            )
-            issues.append(document)
-        return issues
 
 
 @app.post("/load_data/", response_model=List[Document])
