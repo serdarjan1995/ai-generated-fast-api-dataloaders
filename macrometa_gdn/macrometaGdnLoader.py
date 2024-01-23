1abe0c7b7123ab2fc38b8e6acc92f3f5c58cdd3c
Serhii Kosovan <koso.serhii@gmail.com>
2024-01-23 03:17:05 -0500

    fix: macrometaGdnLoader

--- macrometa_gdn/macrometaGdnLoader.py
+++ macrometa_gdn/macrometaGdnLoader.py
@@ -1,9 +1,8 @@
 from typing import List
-from fastapi import FastAPI, HTTPException, Depends, Security
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security.api_key import APIKeyHeader, APIKey
 from pydantic import BaseModel
-import requests
-import json
+import llama_hub.macrometa_gdn as MacrometaGDNReader
 
 app = FastAPI(openapi_url="/api/v1/openapi.json")
 
@@ -17,46 +16,6 @@ async def get_api_key(api_key_header: str = Depends(API_KEY_HEADER)):
 class Document(BaseModel):
     text: str
     extra_info: dict
-
-
-class MacrometaGDNReader:
-    def __init__(self, url: str, apikey: str):
-        self.url = url
-        self.apikey = apikey
-
-    def load_data(self, collection_list: List[str]) -> List[Document]:
-        if not collection_list:
-            raise ValueError("Must specify collection name(s)")
-        results = []
-        for collection_name in collection_list:
-            documents = json.loads(self._load_collection(collection_name))
-            for doc in documents:
-                results.append(
-                    Document(text=doc, extra_info={"collection_name": collection_name})
-                )
-        return results
-
-    def _load_collection(self, collection_name: str) -> str:
-        url = f"{self.url}/_fabric/_system/_api/cursor"
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
-            "Authorization": f"apikey {self.apikey}",
-        }
-        data = {
-            "batchSize": 1000,
-            "ttl": 60,
-            "query": f"FOR doc IN {collection_name} RETURN doc",
-        }
-        response = requests.post(url, headers=headers, data=json.dumps(data))
-        response_json = response.json()
-        if response.status_code == 201:
-            all_documents = response_json.get("result", [])
-        else:
-            raise HTTPException(
-                status_code=response.status_code, detail="Failed to load documents"
-            )
-        return str(all_documents)
 
 
 @app.post("/load_data/", response_model=List[Document])
