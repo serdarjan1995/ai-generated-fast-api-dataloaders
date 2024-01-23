01c8b2ddc1d8813bd743d837d7690e1c564ad8b6
Serhii Kosovan <koso.serhii@gmail.com>
2024-01-23 03:22:21 -0500

    fix: memosLoader

--- memos/memosLoader.py
+++ memos/memosLoader.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Optional
-from fastapi import FastAPI, HTTPException, Query, Depends
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
-import requests
+import llama_hub.memos as MemosReader
 
 app = FastAPI(openapi_url="/api/v1/openapi.json")
 
@@ -9,38 +9,6 @@ app = FastAPI(openapi_url="/api/v1/openapi.json")
 class Document(BaseModel):
     text: str
     extra_info: Dict
-
-
-class MemosReader:
-    def __init__(self, host: str):
-        self._memoUrl = host.rstrip("/") + "/api/memo"
-
-    def load_data(self, params: Optional[Dict] = None):
-        if params is None:
-            params = {}
-        realUrl = self._memoUrl + ("/all" if not params else "")
-        try:
-            response = requests.get(realUrl, params=params)
-            response.raise_for_status()
-            data = response.json()
-        except requests.RequestException as e:
-            raise HTTPException(status_code=400, detail=str(e))
-
-        if "data" not in data:
-            raise HTTPException(status_code=500, detail="Invalid Memo response")
-
-        documents = [
-            Document(
-                text=memo["content"],
-                extra_info={
-                    "creator": memo["creator"],
-                    "resource_list": memo["resourceList"],
-                    id: memo["id"],
-                },
-            )
-            for memo in data["data"]
-        ]
-        return documents
 
 
 def get_loader(host: str):
