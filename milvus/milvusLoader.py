6ead7c59c2e7ae119d0064c5bcc2dc6205ca8699
Serhii Kosovan <koso.serhii@gmail.com>
2024-02-01 10:58:10 -0500

    fix: requirments & code in Milvus

--- milvus/milvusLoader.py
+++ milvus/milvusLoader.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends, Body
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from typing import List
 from fastapi.security.api_key import APIKeyHeader
